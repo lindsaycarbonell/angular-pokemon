698d9d1ef96bcbc7a9ca23bcc0dfe4fefca9b077
Lindsay Carbonell <lindsaycarbonell@gmail.com>
2016-02-06 16:02:50 -0500

    having issues with looping through pokeTypes array

--- js/app.js
+++ js/app.js
@@ -28,119 +28,6 @@ var data = [
       },
 
       {
-        dex_num: 2,
-        name: "Ivysaur",
-        types: [
-          "Grass",
-          "Poison"
-        ],
-        location: "unknown",
-        evolut_chain: [
-          1,
-          2,
-          3
-        ],
-        base_hp: 60,
-        base_atk: 62,
-        base_def: 63,
-        base_spatk: 80,
-        base_spdef: 80,
-        base_speed: 60,
-        isCaught: false
-
-      },
-
-      {
-        dex_num: 3,
-        name: "Venusaur",
-        types: [
-          "Grass",
-          "Poison"
-        ],
-        location: "unknown",
-        evolut_chain: [
-          1,
-          2,
-          3
-        ],
-        base_hp: 80,
-        base_atk: 82,
-        base_def: 83,
-        base_spdef: 100,
-        base_spatk: 100,
-        base_speed: 80,
-        isCaught: false
-
-      },
-
-      {
-        dex_num: 3,
-        name: "Venusaur",
-        types: [
-          "Grass",
-          "Poison"
-        ],
-        location: "unknown",
-        evolut_chain: [
-          1,
-          2,
-          3
-        ],
-        base_hp: 80,
-        base_atk: 82,
-        base_def: 83,
-        base_spatk: 100,
-        base_spdef: 50,
-        base_speed: 80,
-        isCaught: false
-
-      },
-
-      {
-        dex_num: 4,
-        name: "Charmander",
-        types: [
-          "Fire"
-        ],
-        location: "unknown",
-        evolut_chain: [
-          4,
-          5,
-          6
-        ],
-        base_hp: 39,
-        base_atk: 52,
-        base_def: 43,
-        base_spatk: 50,
-        base_spdef: 50,
-        base_speed: 65,
-        isCaught: false
-
-      },
-
-      {
-        dex_num: 5,
-        name: "Charmeleon",
-        types: [
-          "Fire"
-        ],
-        location: "unknown",
-        evolut_chain: [
-          4,
-          5,
-          6
-        ],
-        base_hp: 58,
-        base_atk: 64,
-        base_def: 58,
-        base_spatk: 65,
-        base_spdef: 65,
-        base_speed: 80,
-        isCaught: false
-
-      },
-
-      {
         dex_num: 6,
         name: "Charizard",
         types: [
@@ -161,76 +48,37 @@ var data = [
         base_speed: 100,
         isCaught: false
 
-      },
-
-      {
-        dex_num: 7,
-        name: "Squirtle",
-        types: [
-          "Water"
-        ],
-        location: "unknown",
-        evolut_chain: [
-          7,
-          8,
-          9
-        ],
-        base_hp: 44,
-        base_atk: 48,
-        base_def: 65,
-        base_spatk: 50,
-        base_spdef: 64,
-        base_speed: 43,
-        isCaught: false
-
-      },
-
-      {
-        dex_num: 8,
-        name: "Wartortle",
-        types: [
-          "Water"
-        ],
-        location: "unknown",
-        evolut_chain: [
-          7,
-          8,
-          9
-        ],
-        base_hp: 59,
-        base_atk: 63,
-        base_def: 80,
-        base_spatk: 65,
-        base_spdef: 80,
-        base_speed: 58,
-        isCaught: false
-
-      },
-
-      {
-        dex_num: 9,
-        name: "Blastoise",
-        types: [
-          "Water"
-        ],
-        location: "unknown",
-        evolut_chain: [
-          7,
-          8,
-          9
-        ],
-        base_hp: 79,
-        base_atk: 83,
-        base_def: 100,
-        base_spatk: 85,
-        base_spdef: 105,
-        base_speed: 78,
-        isCaught: false
-
       }
+
 ]
 
+
 this.pokemon = data;
+
+this.printTypes = function(pokeTypes){
+  var printedType = "";
+  console.log("print types: " + pokeTypes);
+
+  for(var i=0; i<pokeTypes.length; i++){
+    thisType = pokeTypes[i];
+    if(thisType=="Fire"){
+      printedType += "Fire";
+    }
+    else if(thisType=="Water") {
+      printedType += "Water";
+    }
+    else if(thisType=="Flying"){
+      printedType += "Flying";
+    }
+    else if(thisType == "Grass"){
+      printedType += "Grass";
+    }
+
+
+    return printedType;
+  }
+
+}
 
 
 
